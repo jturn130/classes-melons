532519fc6aebb9a0c92d12ec476a91baaec82f4d
Hackbright Student <info@hackbrightacademy.com>
2015-09-30 16:25:20 -0700

    first commit

---
+++ melons.py
@@ -0,0 +1,73 @@
+"""This file should have our melon-type classes in it."""
+
+
+class WatermelonOrder(object):
+    species = "Watermelon"
+    color = "green"
+    imported = False
+    shape = 'natural'
+    seasons = ['Fall', 'Summer']
+
+    def get_price(self, qty):
+        """Calculate price, given a number of melons ordered."""
+
+        total = 0   # TODO, calculate the real amount!
+
+        return total
+
+class CantaloupeOrder(object):
+    species = "Cantaloupe"
+    color = "tan"
+    imported = False
+    shape = "natural"
+    seasons = ['Spring', 'Summer']
+
+
+class CasabaOrder(object):
+    species = "Casaba"
+    color = "green"
+    imported = True
+    shape = 'natural'
+    seasons = ['Spring', 'Summer']
+
+class SharlynOrder(object):
+    species = "Sharlyn"
+    color = "tan"
+    imported = True
+    shape = "natural"
+    seasons = ["Summer"]
+
+class SantaClausOrder(object):
+    species = "Santa Claus"
+    color = "green"
+    imported = True
+    shape = "natural"
+    seasons = ['Winter', 'Spring']
+
+class ChristmasOrder(object):
+    species = "Christmas"
+    color = "green"
+    imported = False
+    shape = "natural"
+    seasons = ["Winter"]
+
+class HornedMelonOrder(object):
+    species = "Horned Melon"
+    color = "yellow"
+    imported = True
+    shape = "natural"
+    seasons = ["Summer"]
+
+class XiguaOrder(object):
+    species = "Xigua"
+    color = "black"
+    imported = True
+    shape = "square"
+    seasons = ['Summer']
+
+class OgenOrder(object):
+    species = "Ogen"
+    color = "tan"
+    imported = False
+    shape = "natural"
+    seasons = ["Spring","Summer"]
